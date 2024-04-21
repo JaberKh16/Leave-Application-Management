f4c1055118e794cbdbac4e1ddb453b2632efaf61
Jaber Khan <md.jaberkhan66@gmail.com>
2024-04-21 07:35:51 +0600

    project-files

--- app/Models/Backend/Leave.php
+++ app/Models/Backend/Leave.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Leave extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'leave_type',
+        'start_date',
+        'end_date',
+        'reason_leave',
+        'user_id',
+        'review_status'
+    ];
 }
